4c6a8320a5f21a60f5413bda473608fee4f87c36
claudiney63 <claudineyryan63@gmail.com>
2022-06-24 15:01:29 -0300

    JSX, imports de imgs, e consts e variaveis aula04

--- app_01/app_01/src/App.js
+++ app_01/app_01/src/App.js
@@ -25,15 +25,37 @@ function App() {
 export default App;*/
 
 import React from 'react' //aqui eu importo a biblioteca react
+import Naruto01 from './components/imgs/unnamed.png'
+//eu dou nome a uma imagem e importo ela apartir do caminho em especifico
 
 export default function App() { //na mesma linha eu ja mando importar
+
+  const motivacao = () => {
+    return "Vamos estudar, to sem criatividade"
+  }
+
+  const NOME = "Claudiney Ryan"
+  const DIA = "24/06/2022"
+
   return ( //eu só posso retornar um componente
     //todos os componentes precisam está em um unico só
     //não posso ter tags não finalizadas
+
+    //podemos importar diretamete uma imagem de fora do src,
+    //apartir da pasta public, que está fora do app
     <>
-    <p> Estudos de React</p>
-    <br/>
-    <p> Primeiro dia de Estudo</p>
+      <header>
+        <p>{'Estudos de React por ' + NOME}</p>
+        <br />
+        <p> Primeiro dia de Estudo ({DIA})</p>
+        <br />
+        <p>{motivacao()}</p>
+      </header>
+      <section>
+        <img src={Naruto01} />
+        <img src='/Uzumaki.Naruto.full.3623665.jpg' />
+      </section>
     </>
+
   )
 }
