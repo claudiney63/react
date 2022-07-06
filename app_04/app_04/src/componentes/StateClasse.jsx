8ad35f9a76fe1ccd9f5c045f07c90f87e8d2abca
claudiney63 <claudineyryan63@gmail.com>
2022-07-05 23:32:12 -0300

    Manipulando STATE em Componentes de Classe Aula 23

--- app_04/app_04/src/componentes/StateClasse.jsx
+++ app_04/app_04/src/componentes/StateClasse.jsx
@@ -11,15 +11,38 @@ export default class StateClasse extends React.Component {
     }
 
     ligarEDesligar() {
-        this.setState({ ligado: !(this.state.ligado) })
+        //this.setState({ ligado: !(this.state.ligado) })
+
+        //forma mais segura de chamar e atualizar o setState
+        //pois as vezes acontece erros na chamada do setState
+        this.setState (
+            (state) => ({
+                ligado: !state.ligado
+            })
+        )
     }
+
+    acelerar() {
+        //usar a função dentro de setState, pode otimizar o uso ]
+        //de numeros na atualização da pagina
+        this.setState(
+            (state, props) => (
+                {velocidade: state.velocidade + props.fator}
+            )
+        )
+    }
+
+    //os dois modelos de utilização são validos, se um der problema, pode usar o outro
 
     render() {
         return (
             <>
                 <p>Marca do Carro: {this.props.marca}</p>
                 <p>Ligado: {this.state.ligado ? 'Sim' : 'Não'}</p>
-                <button onClick={() => this.ligarEDesligar()}>{this.state.ligado ? 'Desligar' : 'Ligar'} </button>
+                <button onClick={() => this.ligarEDesligar()}>{this.state.ligado ? 'Desligar' : 'Ligar'} </button><br/>
+
+                <p>Velocidade: {this.state.velocidade}</p>
+                <button onClick={() => this.acelerar()}> Acelerar </button>
             </>
         )
     }
